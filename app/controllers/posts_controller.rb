809b9ccdd8d9d84c3cfdc6ed61001580c8ea7a03
gavroshcho <nikolaivylchanov@gmail.com>
2017-05-09 15:25:55 +0300

    Integrate devise

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,26 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
+  def index
+    @posts = Post.order(created_at: :desc)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.create(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private 
+
+  def post_params
+    params.require(:post).permit(:name, :url)
+  end
+end
